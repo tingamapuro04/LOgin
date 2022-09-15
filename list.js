2bcc383fc2c4a41ba28059f69c4ffe0b0dd5f9f8
tingamapuro04 <alponcemobutu100@gmail.com>
2022-09-15 22:36:35 +0300

    Display people to the UI

--- list.js
+++ list.js
@@ -1,15 +1,38 @@
-const table = document.getElementById('table')
 
-const data = JSON.parse(localStorage.getItem('Personal Details'))
+console.log('mobutu')
+// const data = JSON.parse(localStorage.getItem('Personal Details'))
 
-data.map(item => {
-  const row = document.createElement('tr')
-  row.className = 'row'
-  row.innerHTML += `
-  <td>item.first</td>
-  <td>item.last</td>
-  <td>item.email</td>
-  `
-})
 
-table.appendChild(row)
+// Display items to the UI
+class UI {
+  static displaypeople () {
+    const people = [
+      {
+        first: 'Alphonce',
+        last: 'Mobutu',
+        email: 'alponcemobutu100@gmail.com'
+      },
+      {
+        first: 'Troon',
+        last: 'Onyango',
+        email: 'johntroon001@gmal.com'
+      }
+    ]
+    people.map(item => UI.addto(item))
+  }
+
+  static addto(item) {
+    const table = document.getElementById('table')
+    const row = document.createElement('tr')
+    row.innerHTML = `
+      <td>${item.first}</td>
+      <td>${item.last}</td>
+      <td>${item.email}</td>
+    `
+    table.appendChild(row)
+  }
+
+
+}
+
+document.addEventListener('DOMContentLoaded', UI.displaypeople)
